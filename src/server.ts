3b8f0f6bd00e969af3d1e0831ec228205c1f7e49
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-05-15 10:26:54 -0300

    chore: learning about query and params

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,20 @@ import express from "express";
 const PORT = 3333;
 
 const app = express();
-
+app.use(express.json());
 app.get("/", (request, response) => {
-  response.send(`Hello world Express!`);
+  response.send("Hello World Express");
 });
 
-app.listen(PORT, () => console.log(`server is running  port ${PORT}`));
+app.get("/products/", (request, response) => {
+  response.send(`Products`);
+});
+
+app.post("/products", (request, response) => {
+  const { name, price } = request.body;
+  response.status(201).json({ name, price });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running na porta ${PORT}`);
+});
