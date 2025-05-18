9652069d5fd4ae70f19a136e406180c659c9913a
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-05-17 21:30:22 -0300

    chore: inserted validation with conditional

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { AppError } from "../utils/AppError";
 
 export class ProductsController {
   index(request: Request, response: Response) {
@@ -8,6 +9,22 @@ export class ProductsController {
 
   create(request: Request, response: Response) {
     const { name, price } = request.body;
+    if (!name) {
+      throw new AppError("Nome do produto é obrigatório!");
+    }
+    if (name.trim().length < 6) {
+      throw new AppError(
+        "O nome do produto precisa ter pelo menos 6 caracteres"
+      );
+    }
+    if (!price) {
+      throw new AppError("Preço do produto é  obrigatório!");
+    }
+    if (price < 0) {
+      throw new AppError("Preço do produto não pode ser menor que zero ");
+    }
+    // throw new Error ("Erro ao tentar criar o produto")
+    //throw new AppError("Erro ao tentar criar um produto!");
     response.status(201).json({ name, price, user_id: request.user_id });
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { routes } from "./routes";
+import { AppError } from "./utils/AppError";
 
 const PORT = 3333;
 
@@ -7,6 +8,14 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running na porta ${PORT}`);
-});
+//Tratando exceção de erros
+app.use(
+  (error: any, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({ message: error.message });
+    }
+    response.status(500).json({ message: error.message });
+  }
+);
+
+app.listen(PORT, () => console.log(`Server is running na porta ${PORT}`));
